bd1abcfe3549c1de914b562e8d8188dc6a818730
Kyle Teague <kyteague@gmail.com>
2020-04-29 22:37:46 -0700

    Add basic interfaces

--- rtp.go
+++ rtp.go
@@ -1,2 +1,21 @@
 // Package rtp provides RTP packetizer and depacketizer
 package rtp
+
+import (
+	"io"
+)
+
+type ReadStream interface {
+	io.ReadCloser
+}
+
+type WriteStream interface {
+	io.Writer
+	WriteRTP(header *Header, payload []byte) (int, error)
+}
+
+type Session interface {
+	AcceptStream() (ReadStream, uint32, error)
+	OpenReadStream(ssrc uint32) (ReadStream, error)
+	OpenWriteStream() (WriteStream, error)
+}
